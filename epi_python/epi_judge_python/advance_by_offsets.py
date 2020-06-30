0206d0a8ca340a32ab1678b40fd2038f8ad6c9de
Ming Kong <mkongg92@gmail.com>
2020-06-30 15:10:06 -0400

    can reach end

--- epi_python/epi_judge_python/advance_by_offsets.py
+++ epi_python/epi_judge_python/advance_by_offsets.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    furthest = 0
+    index, length = 0, len(A)
+    while furthest < length and index <= furthest:
+        furthest = max(A[index] + index, furthest)
+        index += 1
+    return furthest >= (length - 1)
+    
 
 
 if __name__ == '__main__':
