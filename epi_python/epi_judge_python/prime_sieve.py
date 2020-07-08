11b97fd234fc166b3a9ddc10865a8399fe07ff2b
Ming Kong <mkongg92@gmail.com>
2020-07-08 16:29:03 -0400

    finish prime sieve

--- epi_python/epi_judge_python/prime_sieve.py
+++ epi_python/epi_judge_python/prime_sieve.py
@@ -29,6 +29,26 @@ from test_framework import generic_test
 #     return primes
 
 
+def generate_primes(n: int) -> List[int]:
+    if n < 2:
+        return []
+    size = (n - 3) // 2 + 1
+    primes = [2]
+    # is_prime[i] reprsents (2i + 3) is prime or not.
+    # For example, is_prime[0] represents 3 is prime or not, is_prime[1] represents 5, is_prime[2] represents 7, etc
+    # Initially set each to true, Then use sieving to eliminate nonprimes.
+    is_prime = [True] * size
+    for i in range(size):
+        if is_prime[i]:
+            p = i * 2 + 3
+            primes.append(p)
+            # Sieving from p^2, where p^2 = (4i^2 + 12i + 9).
+            # The index in is_prime is (2i62 + 6i + 3) because is_prime[i] represents 2i + 3.
+            # Note that we need to use long for j because p^2 mightr overflow.
+            for j in range(2 * i**2 + 6 * i + 3, size, p):
+                is_prime[j] = False
+    return primes
+
 
 if __name__ == '__main__':
     exit(
