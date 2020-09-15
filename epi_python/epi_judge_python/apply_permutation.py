f6b50ddd2ff162f21e0ce934abaec684fb881326
Ming Kong <mkongg92@gmail.com>
2020-09-15 15:27:08 -0400

    epi permutation

--- epi_python/epi_judge_python/apply_permutation.py
+++ epi_python/epi_judge_python/apply_permutation.py
@@ -1,11 +1,20 @@
 from typing import List
 
 from test_framework import generic_test
+import copy
 
+# brute force method
+# def apply_permutation(perm: List[int], A: List[int]) -> None:
+#     temp = [None] * len(perm)
+#     for index, value in enumerate(perm):
+#         temp[value] = A[index]
+#     A = temp
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    for i in range(len(A)):
+        while perm[i] != i:
+            A[perm[i]], A[i] = A[i], A[perm[i]]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
 
 
 def apply_permutation_wrapper(perm, A):
