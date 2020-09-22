2d0e8d48a119e084ba4dc3982a26e13506a68dd4
Ming Kong <mkongg92@gmail.com>
2020-09-22 16:40:23 -0400

    exercism: word count

--- exercism-python/python/word-count/word_count.py
+++ exercism-python/python/word-count/word_count.py
@@ -1,2 +1,20 @@
+import re
+from collections import Counter
+
 def count_words(sentence):
-    pass
+    words = []
+    counter = {}
+
+    for letter in sentence.lower():
+        if letter.isalnum() or letter == "'":
+            words.append(letter)
+        else:
+            words.append(' ')
+
+    for word in ''.join(words).split():
+        word = word.strip("'")
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+    return counter
